6e636d9674a64903cba00f17c89f78532ff227d9
pimpimshop33-dotcom <pimpimshop33@gmail.com>
2026-03-09 00:59:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── FANTÔME Service Worker ──────────────────────────────
-const CACHE_NAME = 'fantome-v8';
+const CACHE_NAME = 'fantome-v9';
 const STATIC = ['/fantome/', '/fantome/index.html', '/fantome/manifest.json'];
 
 // ── INSTALL ─────────────────────────────────────────────
