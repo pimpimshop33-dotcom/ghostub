1f09be6822e8532fc5e5dee2ec49c52560e0239d
pimpimshop33-dotcom <pimpimshop33@gmail.com>
2026-06-17 19:00:22 +0200

    Update sw2.js

--- sw2.js
+++ sw2.js
@@ -1,5 +1,5 @@
 // ── GHOSTUB Service Worker ──────────────────────────────
-const CACHE_NAME = 'ghostub-v28';
+const CACHE_NAME = 'ghostub-v29';
 
 // ── INSTALL — pré-cacher uniquement les assets non versionnés ─
 self.addEventListener('install', e => {
